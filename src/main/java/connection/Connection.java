6dea54b4889c7335091798ad168c03a79edf7b93
Dixie Dean <d.w.sokolov26@gmail.com>
2023-07-19 14:59:55 +0300

    Connection made runnable

--- src/main/java/connection/Connection.java
+++ src/main/java/connection/Connection.java
@@ -3,35 +3,24 @@ package connection;
 import java.io.*;
 import java.net.Socket;
 
-public class Connection {
+public class Connection implements Runnable {
     private final Socket socket;
-    private final BufferedReader in;
-    private final BufferedWriter out;
+    private final BufferedReader reader;
+    private final BufferedWriter writer;
     private final ConnectionObserver observer;
-    private Thread inputThread;
 
     public Connection(ConnectionObserver observer, Socket socket) throws IOException {
         this.observer = observer;
         this.socket = socket;
-        in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
-        inputThread = new Thread(() -> {
-            try {
-                observer.connectionEstablished(this);
-                while (socket.isConnected()) {
-                    observer.messageReceived(in.readLine());
-                }
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        inputThread.start();
+        reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        writer = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+        observer.connectionEstablished(this);
     }
 
     public synchronized void sendMessage(String message) {
         try {
-            out.write(message + "\r\n");
-            out.flush();
+            writer.write(message + "\r\n");
+            writer.flush();
         } catch (IOException exception) {
             observer.exceptionOccurred(this, exception);
             disconnect();
@@ -41,17 +30,28 @@ public class Connection {
     public synchronized void disconnect() {
         observer.disconnection(this);
         try {
-            if (in != null) {
-                in.close();
+            if (reader != null) {
+                reader.close();
             }
-            if (out != null) {
-                out.close();
+            if (writer != null) {
+                writer.close();
             }
             if (socket != null) {
                 socket.close();
             }
         } catch (IOException exception) {
             observer.exceptionOccurred(this, exception);
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (socket.isConnected()) {
+                observer.messageReceived(reader.readLine());
+            }
+        } catch (IOException e) {
+            disconnect();
         }
     }
 
